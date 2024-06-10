7b590c2c6b4f0cf6ad1e5be0e4859483f14a742d
w1y0n0 <wiyono30@gmail.com>
2024-06-10 11:28:35 +0700

    Method/Verb Request

--- server.js
+++ server.js
@@ -9,9 +9,26 @@ const http = require('http');
  */
 const requestListener = (request, response) => {
     response.setHeader('Content-Type', 'text/html');
-
     response.statusCode = 200;
-    response.end('<h1>Halo HTTP Server!</h1>');
+    // response.end('<h1>Halo HTTP Server!</h1>');
+    // const method = request.method;
+    const { method } = request;
+
+    if (method === 'GET') {
+        response.end('<h1>Hello!</h1>');
+    }
+
+    if (method === 'POST') {
+        response.end('<h1>Hai!</h1>');
+    }
+
+    if (method === 'PUT') {
+        response.end('<h1>Bonjour!</h1>');
+    }
+
+    if (method === 'DELETE') {
+        response.end('<h1>Salam!</h1>');
+    }
 };
 
 const server = http.createServer(requestListener);
