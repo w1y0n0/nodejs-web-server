bf9aff166b61945e11b32b5236433015891f4cd1
w1y0n0 <wiyono30@gmail.com>
2024-06-10 14:34:19 +0700

    Body Request

--- server.js
+++ server.js
@@ -19,16 +19,27 @@ const requestListener = (request, response) => {
     }
 
     if (method === 'POST') {
-        response.end('<h1>Hai!</h1>');
+        // response.end('<h1>Hai!</h1>');
+        let body = [];
+
+        request.on('data', (chunk) => {
+            body.push(chunk);
+        });
+
+        request.on('end', () => {
+            body = Buffer.concat(body).toString();
+            const { name } = JSON.parse(body);
+            response.end(`<h1>Hai, ${name}!</h1>`);
+        });
     }
 
-    if (method === 'PUT') {
-        response.end('<h1>Bonjour!</h1>');
-    }
+    // if (method === 'PUT') {
+    //     response.end('<h1>Bonjour!</h1>');
+    // }
 
-    if (method === 'DELETE') {
-        response.end('<h1>Salam!</h1>');
-    }
+    // if (method === 'DELETE') {
+    //     response.end('<h1>Salam!</h1>');
+    // }
 };
 
 const server = http.createServer(requestListener);
