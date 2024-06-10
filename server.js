dc817acedb89d834ea9379d53300284375c3e557
w1y0n0 <wiyono30@gmail.com>
2024-06-10 16:13:17 +0700

    Response Status

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const http = require('http');
  */
 const requestListener = (request, response) => {
     response.setHeader('Content-Type', 'text/html');
-    response.statusCode = 200;
+    // response.statusCode = 200;
     // response.end('<h1>Halo HTTP Server!</h1>');
     // const method = request.method;
     const { method, url } = request;
@@ -18,15 +18,18 @@ const requestListener = (request, response) => {
         // TODO 2: logika respons bila url bernilai '/'
         if (method === 'GET') {
             // response bila client menggunakan GET
+            response.statusCode = 200;
             response.end('<h1>Ini adalah homepage</h1>');
         } else {
             // response bila client tidak menggunakan GET
+            response.statusCode = 400;
             response.end(`<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`);
         }
     } else if (url === '/about') {
         // TODO 3: logika respons bila url bernilai '/about'
         if (method === 'GET') {
             // response bila client menggunakan GET
+            response.statusCode = 200;
             response.end('<h1>Halo! Ini adalah halaman about</h1>');
         } else if (method === 'POST') {
             // response bila client menggunakan POST (dengan melampirkan data name pada body)
@@ -39,14 +42,17 @@ const requestListener = (request, response) => {
             request.on('end', () => {
                 body = Buffer.concat(body).toString();
                 const { name } = JSON.parse(body);
+                response.statusCode = 200;
                 response.end(`<h1>Halo, ${name}! Ini adalah halaman about</h1>`);
             });
         } else {
             // response bila client menggunakan selain GET dan POST
+            response.statusCode = 400;
             response.end(`Halaman tidak dapat diakses menggunakan ${method} request`);
         }
     } else {
         // TODO 1: logika respons bila url bukan '/' atau '/about'
+        response.statusCode = 404;
         response.end('<h1>Halaman tidak ditemukan!</h1>');
     }
 
